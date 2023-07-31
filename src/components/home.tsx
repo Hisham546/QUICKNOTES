f73b3e8224ce5522d963edc26dddcce46acf8524
Hisham <hmuhammed546@gmail.com>
2023-07-31 12:49:06 +0530

    removed unwanted file

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect,useRef,useState } from "react";
-import {widthPercentageToDP as wp,heightPercentageToDP as hp} from 'react-native-responsive-screen'
+import React, { useEffect, useRef, useState } from "react";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import {
-View,
-Image,
-Text,Button,PermissionsAndroid,
-StyleSheet,TouchableOpacity,FlatList,ImageBackground,
-TextInput}
-from "react-native";
+  View,
+  Image,
+  Text, Button, PermissionsAndroid,
+  StyleSheet, TouchableOpacity, FlatList, ImageBackground,
+  TextInput
+}
+  from "react-native";
 import CardView from 'react-native-cardview';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import firestore from '@react-native-firebase/firestore';
-import FloatingButton from "./floatingButton/floatingButton";
 import { useTheme } from 'react-native-paper';
 import { useFocusEffect } from '@react-navigation/native';
 interface Note {
@@ -19,97 +19,97 @@ interface Note {
   Heading: string;
 }
 
-export default function Home({navigation}: {navigation: any}){
+export default function Home({ navigation }: { navigation: any }) {
 
-const [notes,setNotes]=useState<Note[]>([]);
-
-
-const image = {uri: 'https://legacy.reactjs.org/logo-og.png'};
+  const [notes, setNotes] = useState<Note[]>([]);
 
 
-useFocusEffect(
-  React.useCallback(() => {
-    async function getNotes(){
- await firestore().collection('notes').get().then((querySnapshot) => {
-    const fetchedNotes: Note[] = []; // Create an empty array to hold the fetched notes
-    querySnapshot.forEach(snapshot => {
-      const data = snapshot.data();
-      const noteData = data as Note;
-      fetchedNotes.push(noteData); // Add the fetched noteData to the array
-    });
+  const image = { uri: 'https://legacy.reactjs.org/logo-og.png' };
 
-    setNotes(fetchedNotes); // Set the state with the complete array of fetched notes
-  });
-}
-getNotes()
-}, [])
-);
 
-  
+  useFocusEffect(
+    React.useCallback(() => {
+      async function getNotes() {
+        await firestore().collection('notes').get().then((querySnapshot) => {
+          const fetchedNotes: Note[] = []; // Create an empty array to hold the fetched notes
+          querySnapshot.forEach(snapshot => {
+            const data = snapshot.data();
+            const noteData = data as Note;
+            fetchedNotes.push(noteData); // Add the fetched noteData to the array
+          });
+
+          setNotes(fetchedNotes); // Set the state with the complete array of fetched notes
+        });
+      }
+      getNotes()
+    }, [])
+  );
 
 
 
 
-return(
+
+
+  return (
 
     <View style={styles.mainContainer}>
-    <View style={styles.headingView}>
-    <MaterialIcon name="feather" size={hp('3.20%')} color="black"  style={{marginLeft:wp('2')}} />
-      <Text style={styles.headerText}> My  Notes</Text>
-      <TouchableOpacity>
-        {/*<Image
+      <View style={styles.headingView}>
+        <MaterialIcon name="feather" size={hp('3.20%')} color="black" style={{ marginLeft: wp('2') }} />
+        <Text style={styles.headerText}> My  Notes</Text>
+        <TouchableOpacity>
+          {/*<Image
            style={styles.userLogo}
            source={image}
 />*/}
-      </TouchableOpacity>
-     </View>
-    <View style={styles.firstView}>
-     <FlatList
-             showsVerticalScrollIndicator={false}
-             numColumns={2}
-             data={notes}
-             style={{backgroundColor:'#F6F4EB',height:hp('100'),width:wp('98.50'),marginLeft:wp('1')}}
-             renderItem={({item}) =>
-           <>
-              <TouchableOpacity activeOpacity={1} onPress={()=> navigation.navigate('WriteNote', { notes: item })} >
-           <CardView
-          cornerRadius={5}
-             style={[styles.noteCard,{backgroundColor:(item.type) === '1' ? 'white':'white'}]}>
-              <View  > 
-                <View style={{width:wp('20'),marginLeft:wp('29.5'),height:hp('3'),justifyContent:'center',alignItems:'center',backgroundColor:(item.type) === 'High Priority' ? '#FE0000':(item.type) === 'Important'? '#7A9D54' : '#D8D9DA',}}>
-                  <Text style={{fontSize:hp('1.20'),color:'white'}}>{item.type}</Text>
-                </View>
-                       <Text style={{fontSize:hp('1.70'),letterSpacing:wp('.10%'),minWidth:wp('15'),marginTop:hp('2'),fontFamily:'Manrope-Bold',color:'black'}}>{item.Heading}</Text>
-               <Text style={{fontSize:hp('1.70'),letterSpacing:wp('.10%'),minWidth:wp('15'),marginTop:hp('2'),fontFamily:'Manrope-Regular',color:'black'}}>{item.Description}</Text>
-                </View>
-           </CardView>
-           </TouchableOpacity>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.firstView}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          data={notes}
+          style={{ backgroundColor: '#F6F4EB', height: hp('100'), width: wp('98.50'), marginLeft: wp('1') }}
+          renderItem={({ item }) =>
+            <>
+              <TouchableOpacity activeOpacity={1} onPress={() => navigation.navigate('WriteNote', { notes: item })} >
+                <CardView
+                  cornerRadius={5}
+                  style={[styles.noteCard, { backgroundColor: (item.type) === '1' ? 'white' : 'white' }]}>
+                  <View  >
+                    <View style={{ width: wp('20'), marginLeft: wp('29.5'), height: hp('3'), justifyContent: 'center', alignItems: 'center', backgroundColor: (item.type) === 'High Priority' ? '#FE0000' : (item.type) === 'Important' ? '#7A9D54' : '#D8D9DA', }}>
+                      <Text style={{ fontSize: hp('1.20'), color: 'white' }}>{item.type}</Text>
+                    </View>
+                    <Text style={{ fontSize: hp('1.70'), letterSpacing: wp('.10%'), minWidth: wp('15'), marginTop: hp('2'), fontFamily: 'Manrope-Bold', color: 'black' }}>{item.Heading}</Text>
+                    <Text style={{ fontSize: hp('1.70'), letterSpacing: wp('.10%'), minWidth: wp('15'), marginTop: hp('2'), fontFamily: 'Manrope-Regular', color: 'black' }}>{item.Description}</Text>
+                  </View>
+                </CardView>
+              </TouchableOpacity>
 
-         </>
-              }
+            </>
+          }
 
-            />
-
-
-         
-<View style={styles.secondView}>
-<TouchableOpacity activeOpacity={.9} style={styles.writeButton} onPress={() => navigation.navigate('WriteNote')}>       
-    <MaterialIcon name="feather" size={hp('3.20%')} color="white"  />
-   
- </TouchableOpacity>    
-             
-               </View>
-  
-           
-           </View>
-         
-          
-</View>
+        />
 
 
 
+        <View style={styles.secondView}>
+          <TouchableOpacity activeOpacity={.9} style={styles.writeButton} onPress={() => navigation.navigate('WriteNote')}>
+            <MaterialIcon name="feather" size={hp('3.20%')} color="white" />
 
-)
+          </TouchableOpacity>
+
+        </View>
+
+
+      </View>
+
+
+    </View>
+
+
+
+
+  )
 
 
 
@@ -119,94 +119,94 @@ return(
 
 const styles = StyleSheet.create({
 
-mainContainer:{
-  flex: 1,
-backgroundColor:'#F6F4EB'
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#F6F4EB'
 
-},
-headingView:{
-backgroundColor:'white',
-width:wp('100'),
-height:hp('8'),
-//justifyContent:'space-evenly',
-flexDirection:'row',
-alignItems:'center'
+  },
+  headingView: {
+    backgroundColor: 'white',
+    width: wp('100'),
+    height: hp('8'),
+    //justifyContent:'space-evenly',
+    flexDirection: 'row',
+    alignItems: 'center'
 
-},
-firstView:{
-  width:wp('98'),
-  height:hp('92'),
-  backgroundColor:'white',
-  justifyContent:'flex-end'
+  },
+  firstView: {
+    width: wp('98'),
+    height: hp('92'),
+    backgroundColor: 'white',
+    justifyContent: 'flex-end'
 
 
-},
-secondView:{
-   width:wp('100'),
-   height:hp('8'),
-   flexDirection:'row',
-   justifyContent:'center',
-   backgroundColor:'#F6F4EB',
+  },
+  secondView: {
+    width: wp('100'),
+    height: hp('8'),
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: '#F6F4EB',
 
-},
-headerText:{
-color:'black',
-fontFamily:'Manrope-Bold',
-fontSize:hp('2.20'),
-marginLeft:wp('2')
-},
-userLogo:{
-  width:30,
-  height:30,
-  borderRadius:15,
-  marginRight:wp(3)
-  
-  
-    },
+  },
+  headerText: {
+    color: 'black',
+    fontFamily: 'Manrope-Bold',
+    fontSize: hp('2.20'),
+    marginLeft: wp('2')
+  },
+  userLogo: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    marginRight: wp(3)
+
+
+  },
 
   item: {
-     height : hp('27%'),
-     width : wp(' 50%'),
-     backgroundColor:'orange',
-     borderRadius:8,
-     shadowColor: '#000000',
-     shadowOffset: { width: 0, height: 1 },
-     shadowOpacity: 0.9,
-     shadowRadius: 3,
-     elevation: 6,
-     marginRight:wp('1'),
-     marginLeft: wp('1%'),
-     marginTop:hp('.80%'),
-     marginBottom:hp('.50')
+    height: hp('27%'),
+    width: wp(' 50%'),
+    backgroundColor: 'orange',
+    borderRadius: 8,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.9,
+    shadowRadius: 3,
+    elevation: 6,
+    marginRight: wp('1'),
+    marginLeft: wp('1%'),
+    marginTop: hp('.80%'),
+    marginBottom: hp('.50')
   },
-  writeButton:{
-    width:50,
-    height:50,
-    borderRadius:25,
-    backgroundColor:'#068FFF',
-    justifyContent:'space-evenly',
-    alignItems:'center',
-    marginRight:wp('5'),
-    flexDirection:'row',
-    position: 'absolute',  
+  writeButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#068FFF',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginRight: wp('5'),
+    flexDirection: 'row',
+    position: 'absolute',
 
 
   },
 
   noteCard: {
-    height : hp('27%'),
-    width : wp(' 50%'),
-   // backgroundColor:'#f4f9ff',
-    borderRadius:8,
+    height: hp('27%'),
+    width: wp(' 50%'),
+    // backgroundColor:'#f4f9ff',
+    borderRadius: 8,
 
-   // elevation: 6,
-  //  borderWidth:wp('5'),
-  //  borderColor:'black',
-    marginRight:wp('1'),
+    // elevation: 6,
+    //  borderWidth:wp('5'),
+    //  borderColor:'black',
+    marginRight: wp('1'),
 
-    marginTop:hp('.80%'),
-    marginBottom:hp('.50')
- },
+    marginTop: hp('.80%'),
+    marginBottom: hp('.50')
+  },
 
 
 
--- src/components/writeNote.tsx
+++ src/components/writeNote.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect,useRef,useState } from "react";
-import {widthPercentageToDP as wp,heightPercentageToDP as hp} from 'react-native-responsive-screen'
+import React, { useEffect, useRef, useState } from "react";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import {
-View,
-Image,
-Text,Button,PermissionsAndroid,
-StyleSheet,TouchableOpacity,FlatList,ImageBackground,
-TextInput,Dimensions}
-from "react-native";
+  View,
+  Image,
+  Text, Button, PermissionsAndroid,
+  StyleSheet, TouchableOpacity, FlatList, ImageBackground,
+  TextInput, Dimensions
+}
+  from "react-native";
 import CardView from 'react-native-cardview';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import firestore from '@react-native-firebase/firestore';
@@ -14,18 +15,18 @@ import firebase from '@react-native-firebase/app';
 import Toast from "react-native-simple-toast";
 import { Dropdown } from 'react-native-element-dropdown';
 
-export default function WriteNote ({navigation}: {navigation: any},{route}:{route:any}){
+export default function WriteNote({ navigation }: { navigation: any }, { route }: { route: any }) {
 
-     const usersCollection = firestore().collection('notes');
+  const usersCollection = firestore().collection('notes');
 
-   const windowWidth = Dimensions.get('window').width;
-    const windowHeight = Dimensions.get('window').height;
-    const [title, onChangeTitle] = React.useState('');
-    const [note, onChangeNote] = React.useState('');
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+  const [title, onChangeTitle] = React.useState('');
+  const [note, onChangeNote] = React.useState('');
 
-   // const notesData =route.params ? route.params.notes : null;
-//console.log(notesData)
-const data = [
+  // const notesData =route.params ? route.params.notes : null;
+  //console.log(notesData)
+  const data = [
     { label: 'Normal', value: 'Normal' },
     { label: 'Important', value: 'Important' },
     { label: 'High Priority', value: 'High Priority' },
@@ -33,215 +34,217 @@ const data = [
 
   const [value, setValue] = useState<string>();
 
-    const save= async()=> {
-       if(note == ''){
-           Toast.show('Write something first!.', Toast.LONG);
+  const save = async () => {
+    if (note == '') {
+      Toast.show('Write something first!.', Toast.LONG);
 
-        }else{
+    } else {
       await firestore().collection('notes').doc()
-         .set({
-           type:value,
-           Heading: title,
-           Description: note,
-      })
-      .then(() => {
-              Toast.show('Your note has been saved.', Toast.SHORT);
-       // console.log('Your note has been saved.');
-        //console.log('added');
-      });
-     }
-      }
- 
-
-//       function delay(ms: number | undefined) {
-//         return new Promise(resolve => setTimeout(resolve, ms));
-//       }
-//
-//       // This is an async function that does something asynchronously (bakes a cake in our example).
-//       async function bakeCake() {
-//         console.log("Putting the cake in the oven...");
-//
-//         // This line waits for 2 seconds (the cake is baking).
-//         await delay(2000);
-//
-//         console.log("Cake is ready!");
-//       }
-//
-//
-//    const test=() => {
-//
-//     console.log("Start baking process!");
-//     bakeCake(); // This starts the async process but doesn't wait for it to finish.
-//     console.log("Doing other things..."); // While the cake is baking, we can do other tasks.
-//
-//    }
-//
-
-
-
-
-
-
-    return(
-
-        <View style={styles.mainContainer}>
-           <View style={styles.headingView}>
-            <TouchableOpacity  activeOpacity={1} onPress={()=> navigation.navigate('Home')}>
-           <MaterialIcon name={'arrow-left'} size={hp('3%')} color={'black'} style={{marginLeft:wp('2')}}  />
-           </TouchableOpacity>
-  <Dropdown
-                    style={styles.dropdown}
-                    placeholderStyle={styles.placeholderStyle}
-                    selectedTextStyle={styles.selectedTextStyle}  
-                    containerStyle={{backgroundColor:'white'}} 
-                    itemTextStyle={{color:'black',fontSize:hp('1.50')}}   
-                    data={data}
-                    maxHeight={250}
-                    labelField="label"
-                    valueField="value"
-                    placeholder="Select type"
-                    value={value}
-                    onChange={item => {
-                      setValue(item.value);
-                    }}
-
-                  />
-           <TouchableOpacity style={{width:wp('25'),height:hp('4.5'),borderRadius:4,backgroundColor:'#78C1F3',
-           alignItems:'center',justifyContent:'center',marginRight:wp('5'),flexDirection:'row'}} onPress={()=> save()}>
-            <Text style={{color:'white',fontSize:hp('1.45'),fontFamily:'Manrope-Bold'}}>Save </Text>
-           <MaterialIcon name={'content-save'} size={hp('2.20%')} color={'white'} style={{marginLeft:wp('2')}} />  
-           </TouchableOpacity>
-
-           </View>
-           <TextInput
-                   style={styles.title}
-                   onChangeText={onChangeTitle}
-                   value={title}
-                   placeholderTextColor={'gray'}
-                   placeholder={"Title"}
-                   multiline={true}
-                   textAlignVertical={'top'}
-              />
-
-          <TextInput
-                   style={styles.input}
-                   onChangeText={onChangeNote}
-                   value={note}
-                   multiline={true}
-                   placeholderTextColor={'gray'}
-                   placeholder={"write your note"}
-                   textAlignVertical={'top'}
-              />
-    
-    
-    <View style={styles.secondView}>
-               
-             
-               </View>
-    </View>
-    
-    
-    
-    
-    )
-    
-    
-    
-    
+        .set({
+          type: value,
+          Heading: title,
+          Description: note,
+        })
+        .then(() => {
+          Toast.show('Your note has been saved.', Toast.SHORT);
+          // console.log('Your note has been saved.');
+          //console.log('added');
+        });
     }
-    
-    
- const styles = StyleSheet.create({
-    
-  mainContainer:{
-    flex:1,
-    backgroundColor:'white'
-    
+  }
+
+
+  //       function delay(ms: number | undefined) {
+  //         return new Promise(resolve => setTimeout(resolve, ms));
+  //       }
+  //
+  //       // This is an async function that does something asynchronously (bakes a cake in our example).
+  //       async function bakeCake() {
+  //         console.log("Putting the cake in the oven...");
+  //
+  //         // This line waits for 2 seconds (the cake is baking).
+  //         await delay(2000);
+  //
+  //         console.log("Cake is ready!");
+  //       }
+  //
+  //
+  //    const test=() => {
+  //
+  //     console.log("Start baking process!");
+  //     bakeCake(); // This starts the async process but doesn't wait for it to finish.
+  //     console.log("Doing other things..."); // While the cake is baking, we can do other tasks.
+  //
+  //    }
+  //
+
+
+
+
+
+
+  return (
+
+    <View style={styles.mainContainer}>
+      <View style={styles.headingView}>
+        <TouchableOpacity activeOpacity={1} onPress={() => navigation.navigate('Home')}>
+          <MaterialIcon name={'arrow-left'} size={hp('3%')} color={'black'} style={{ marginLeft: wp('2') }} />
+        </TouchableOpacity>
+        <Dropdown
+          style={styles.dropdown}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          containerStyle={{ backgroundColor: 'white' }}
+          itemTextStyle={{ color: 'black', fontSize: hp('1.50') }}
+          data={data}
+          maxHeight={250}
+          labelField="label"
+          valueField="value"
+          placeholder="Select type"
+          value={value}
+          onChange={item => {
+            setValue(item.value);
+          }}
+
+        />
+        <TouchableOpacity style={{
+          width: wp('25'), height: hp('4.5'), borderRadius: 4, backgroundColor: '#78C1F3',
+          alignItems: 'center', justifyContent: 'center', marginRight: wp('5'), flexDirection: 'row'
+        }} onPress={() => save()}>
+          <Text style={{ color: 'white', fontSize: hp('1.45'), fontFamily: 'Manrope-Bold' }}>Save </Text>
+          <MaterialIcon name={'content-save'} size={hp('2.20%')} color={'white'} style={{ marginLeft: wp('2') }} />
+        </TouchableOpacity>
+
+      </View>
+      <TextInput
+        style={styles.title}
+        onChangeText={onChangeTitle}
+        value={title}
+        placeholderTextColor={'gray'}
+        placeholder={"Title"}
+        multiline={true}
+        textAlignVertical={'top'}
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNote}
+        value={note}
+        multiline={true}
+        placeholderTextColor={'gray'}
+        placeholder={"write your note"}
+        textAlignVertical={'top'}
+      />
+
+
+      <View style={styles.secondView}>
+
+
+      </View>
+    </View>
+
+
+
+
+  )
+
+
+
+
+}
+
+
+const styles = StyleSheet.create({
+
+  mainContainer: {
+    flex: 1,
+    backgroundColor: 'white'
+
   },
-  headingView:{
-    backgroundColor:'white',
-    width:wp('100'),
-    height:hp('8'),
-    justifyContent:'space-between',
-    flexDirection:'row',
-    alignItems:'center'
-    
+  headingView: {
+    backgroundColor: 'white',
+    width: wp('100'),
+    height: hp('8'),
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center'
+
   },
-  headerText:{
-    color:'black',
-    fontFamily:'Manrope-Bold',
-    fontSize:hp('2.20'),
-    marginLeft:wp('2')
- },
- userLogo:{
-      width:30,
-      height:30,
-      borderRadius:15,
-      marginRight:wp(3)
+  headerText: {
+    color: 'black',
+    fontFamily: 'Manrope-Bold',
+    fontSize: hp('2.20'),
+    marginLeft: wp('2')
+  },
+  userLogo: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    marginRight: wp(3)
   },
   item: {
-         height : hp('97%'),
-         width : wp(' 97%'),
-         backgroundColor:'white',
-         borderRadius:8,
-         shadowColor: '#000000',
-         shadowOffset: { width: 0, height: 1 },
-         shadowOpacity: 30,
-         shadowRadius: 3,
-         elevation: 6,
-         marginRight:wp('1'),
-         marginLeft: wp('1.5%'),
-         marginTop:hp('1.5%'),
+    height: hp('97%'),
+    width: wp(' 97%'),
+    backgroundColor: 'white',
+    borderRadius: 8,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 30,
+    shadowRadius: 3,
+    elevation: 6,
+    marginRight: wp('1'),
+    marginLeft: wp('1.5%'),
+    marginTop: hp('1.5%'),
   },
-  writeButton:{
-        width:wp('35'),
-        height:hp('6'),
-        borderRadius:25,
-        backgroundColor:'#068FFF',
-        justifyContent:'space-evenly',
-        alignItems:'center',
-        marginRight:wp('5'),
-        flexDirection:'row'
- },
- firstView:{
-       width:wp('100'),
-       height:hp('80'),
- },
-  secondView:{
-         width:wp('100'),
-         height:hp('12'),
-         flexDirection:'row',
-         justifyContent:'flex-end',
+  writeButton: {
+    width: wp('35'),
+    height: hp('6'),
+    borderRadius: 25,
+    backgroundColor: '#068FFF',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginRight: wp('5'),
+    flexDirection: 'row'
   },
-  input:{
-        height: hp('85%'),
-        width: wp('97%'),
-        color: 'black',
-        fontSize: hp('1.80%'),
-        textAlign:'justify',
-        marginLeft:wp('2'),
- },
-  title:{
-        height: hp('7%'),
-        width: wp('97%'),
-        color: 'black',
-        fontSize: hp('1.80%'),
-        marginLeft:wp('2'),
+  firstView: {
+    width: wp('100'),
+    height: hp('80'),
+  },
+  secondView: {
+    width: wp('100'),
+    height: hp('12'),
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  input: {
+    height: hp('85%'),
+    width: wp('97%'),
+    color: 'black',
+    fontSize: hp('1.80%'),
+    textAlign: 'justify',
+    marginLeft: wp('2'),
+  },
+  title: {
+    height: hp('7%'),
+    width: wp('97%'),
+    color: 'black',
+    fontSize: hp('1.80%'),
+    marginLeft: wp('2'),
 
   },
-   dropdown: {
-      //  margin: 16,
-        height:hp('4'),
-        width:wp('30'),
-      },
-      placeholderStyle: {
-        fontSize:hp('1.50'),
-        color:'black',
-      },
-      selectedTextStyle: {
-        fontSize: hp('1.50'),
-        color:'black',
-        fontFamily:'Manrope-Medium'
-      },
+  dropdown: {
+    //  margin: 16,
+    height: hp('4'),
+    width: wp('30'),
+  },
+  placeholderStyle: {
+    fontSize: hp('1.50'),
+    color: 'black',
+  },
+  selectedTextStyle: {
+    fontSize: hp('1.50'),
+    color: 'black',
+    fontFamily: 'Manrope-Medium'
+  },
 
-  })
+})
